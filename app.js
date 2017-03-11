feb4a57bf35abc07874c657e7f1ad0f41ae463a2
Grant Richmond <mail@grant.codes>
2017-03-11 01:13:57 +0100

    use the micropub-helper library and add photo post dialog

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const builder = require('botbuilder');
 const request = require('request');
-const Microformats = require('microformat-node');
-const qs = require('querystring');
-const emoji = require('node-emoji');
+const Micropub = require('micropub-helper');
+const cleanText = require('./lib/clean-text');
+const cleanUrl = require('./lib/clean-url');
+const micropubPromts = require('./lib/temp');
 
 // Setup express server for html site
 const app = express();
@@ -23,6 +25,12 @@ const appConfig = {
   port: process.env.port || process.env.PORT || 3978,
   url: process.env.URL || 'http://localhost:3978',
 };
+
+let micropub = new Micropub({
+  clientId: appConfig.url,
+  redirectUri: appConfig.url + '/auth',
+  state: 'Super secret value',
+});
 
 app.listen(appConfig.port, () => {
   console.log('%s listening to %s', app.name, appConfig.url);
@@ -72,6 +80,7 @@ bot.dialog('/', new builder.IntentDialog()
   .matchesAny([regexes.quickPost, /^post/i], '/instant-note')
   .matchesAny([regexes.quickJournal, /^journal/i], '/instant-journal')
   .matches(/^advancedpost/i, '/advanced-post')
+  .matches(/^photo/i, '/photo')
   .matches(/^help/i, '/help')
   .matches(/^info/i, '/info')
   .onDefault(builder.DialogAction.send("🤷‍ I'm sorry. I didn't understand."))
@@ -83,41 +92,29 @@ bot.dialog('/instant-note', [
       session.send('Whoa you dont seem to have an access token saved 🔐.');
       session.endDialog('Just type "authenticate" to get started');
     } else {
+      session.dialogData.data = {};
       if (args && args.matched && args.matched[1] && args.matched[1].trim()) {
         let text = args.matched[1].trim();
-        session.dialogData.content = cleanText(text, session.message.source);
-        next();
-      } else {
-        builder.Prompts.text(session, 'What do you want to post? 💬');
+        session.dialogData.data.content = cleanText(text, session.message.source);
       }
-    }
-  },
-  (session, results, next) => {
-    if (results.response) {
-      session.dialogData.content = cleanText(results.response, session.message.source);
-    }
-    if (session.dialogData.content.length > 140) {
-      const extraLength = session.dialogData.content.length - 140;
-      builder.Prompts.confirm(session, 'Whoa 😵 this post is ' + extraLength +' characters too long for twitter. Is that ok?');
-    } else {
       next();
     }
   },
-  (session, results) => {
-    if (results.response === false) {
-      session.endDialog('Ok I have cancelled that post 🗑. Feel free to try again');
-    } else {
-      session.send('Sending post ✉️');
-      micropub(session, {
-        h: 'entry',
-        content: session.dialogData.content,
-      }).then((card) => {
-        session.endDialog(card);
-      }).catch((err) => {
-        session.send('Uh oh 😯. There was an error sending that');
-        session.endDialog(JSON.stringify(err));
-      });
-    }
+  ...micropubPromts.content,
+  (session, results, next) => {
+    session.sendTyping();
+    micropub.options.micropubEndpoint = session.userData.micropub;
+    micropub.options.token = session.userData.accessToken;
+    micropub.create({
+      h: 'entry',
+      content: session.dialogData.data.content,
+    }, 'form').then((url) => {
+      const card = getSuccessCard(session, url, session.dialogData.content);
+      session.endDialog(card);
+    }).catch((err) => {
+      session.send('Uh oh 😯. There was an error sending that');
+      session.endDialog(JSON.stringify(err));
+    });
   }
 ]);
 
@@ -127,25 +124,25 @@ bot.dialog('/instant-journal', [
       session.send('Whoa you dont seem to have an access token saved 🔐.');
       session.endDialog('Just type "authenticate" to get started');
     } else {
+      session.dialogData.data = {};
       if (args && args.matched && args.matched[1] && args.matched[1].trim()) {
         let text = args.matched[1].trim();
-        session.dialogData.content = cleanText(text, session.message.source);
-        next();
-      } else {
-        builder.Prompts.text(session, 'What do you want to journal 📝?');
+        session.dialogData.data.content = cleanText(text, session.message.source);
       }
+      next();
     }
   },
+  ...micropubPromts.content,
   (session, results) => {
-    if (results.response) {
-      session.dialogData.content = cleanText(results.response, session.message.source);
-    }
-    session.send('Sending journal entry ✉️');
-    micropub(session, {
+    session.sendTyping();
+    micropub.options.micropubEndpoint = session.userData.micropub;
+    micropub.options.token = session.userData.accessToken;
+    micropub.create({
       h: 'entry',
-      content: session.dialogData.content,
+      content: session.dialogData.data.content,
       category: ['journal', 'private'],
-    }).then((card) => {
+    }, 'form').then((url) => {
+      const card = getSuccessCard(session, url, session.dialogData.content);
       session.endDialog(card);
     }).catch((err) => {
       session.send('Uh oh 😯. There was an error sending that');
@@ -154,8 +151,49 @@ bot.dialog('/instant-journal', [
   }
 ]);
 
+bot.dialog('/photo', [
+  (session, args, next) => {
+    if (!session.userData.micropub || !session.userData.accessToken) {
+      session.send('Whoa you dont seem to have an access token saved 🔐.');
+      session.endDialog('Just type "authenticate" to get started');
+    } else {
+      session.dialogData.data = {
+        h: 'entry',
+      };
+      next();
+    }
+  },
+  ...micropubPromts.photo,
+  ...micropubPromts.name,
+  ...micropubPromts.content,
+  (session, results, next) => {
+    session.sendTyping();
+    micropub.options.micropubEndpoint = session.userData.micropub;
+    micropub.options.token = session.userData.accessToken;
+    const tempLocation = Math.random().toString(36).substring(7);
+    request(session.dialogData.data.photo)
+      .pipe(fs.createWriteStream(tempLocation))
+      .on('error', () => {
+        session.endDialog('Uh oh 😯. There was an error sending that');
+      })
+      .on('finish', () => {
+        session.sendTyping();
+        let post = Object.assign({}, session.dialogData.data);
+        post.photo = fs.createReadStream(tempLocation);
+        micropub.create(post, 'multipart').then((url) => {
+          fs.unlink(tempLocation);
+          const card = getSuccessCard(session, url, session.dialogData.data.content, session.dialogData.data.name);
+          session.endDialog(card);
+        }).catch((err) => {
+          session.send('Uh oh 😯. There was an error sending that');
+          session.endDialog(JSON.stringify(err));
+        });
+      })
+  }
+]);
+
 bot.dialog('/advanced-post', [
-  (session, next) => {
+  (session, results, next) => {
     if (!session.userData.micropub || !session.userData.accessToken) {
       session.send('Whoa you dont seem to have an access token saved 🔐.');
       session.endDialog('Just type "authenticate" to get started');
@@ -165,90 +203,26 @@ bot.dialog('/advanced-post', [
       if (session.message && session.message.source) {
         session.dialogData.source = session.message.source;
       }
-      builder.Prompts.choice(session, 'What sort of entry are you creating?', ['entry', 'card', 'event', 'cite']);
-    }
-  },
-  (session, results, next) => {
-    session.dialogData.data.h = results.response.entity;
-    builder.Prompts.text(session, 'Add a name (or skip)');
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data.name = cleanText(results.response, session.dialogData.source);
-    }
-    builder.Prompts.text(session, 'Add a summary (or skip)');
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data.summary = cleanText(results.response, session.dialogData.source);
-    }
-    builder.Prompts.text(session, 'Add content (or skip)');
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data.content = cleanText(results.response, session.dialogData.source);
-    }
-    builder.Prompts.time(session, 'Add published time 🕐 (you can say now)');
-  },
-  (session, results, next) => {
-    if (results.response) {
-      session.dialogData.data.published = builder.EntityRecognizer.resolveTime([results.response]);
-    }
-    builder.Prompts.text(session, 'Add categories (space seperated or skip)');
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data.category = results.response.split(' ');
-    }
-    builder.Prompts.text(session, 'Add in-reply-to (or skip)');
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data['in-reply-to'] = cleanUrl(results.response);
-    }
-    builder.Prompts.text(session, 'Add like-of 👍 (or skip)');
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data['like-of'] = cleanUrl(results.response);
-    }
-    builder.Prompts.text(session, 'Add repost-of 🔁 (or skip)');
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data['repost-of'] = cleanUrl(results.response);
-    }
-    builder.Prompts.confirm(session, 'Do you want to add an image 📷 ?');
-  },
-  (session, results, next) => {
-    if (results && results.response) {
-      builder.Prompts.attachment(session, 'Attach an image 🔗');
-    } else {
       next();
     }
   },
+  ...micropubPromts.h,
+  ...micropubPromts.name,
+  ...micropubPromts.summary,
+  ...micropubPromts.content,
+  ...micropubPromts.published,
+  ...micropubPromts.category,
+  ...micropubPromts.inReplyTo,
+  ...micropubPromts.likeOf,
+  ...micropubPromts.repostOf,
+  ...micropubPromts.photoConfirm,
+  ...micropubPromts.mpSyndicateTo,
   (session, results, next) => {
-    if (results && results.response && results.response[0] && results.response[0].contentUrl) {
-      // results.response[0].name;
-      // results.response[0].contentType;
-      // results.response[0].contentUrl;
-      session.dialogData.data.photo = results.response[0].contentUrl;
-    }
-    next();
-  },
-  (session, results, next) => {
-    getSyndication(session).then((options) => {
-      builder.Prompts.text(session, 'Any sydication options? Write any of ' + options.join(' ') + ' (space seperated or skip)');
-    }).catch(() => {
-      next();
-    });
-  },
-  (session, results, next) => {
-    if (results && results.response && results.response != 'skip') {
-      session.dialogData.data['mp-syndicate-to'] = cleanText(results.response, session.message.source).split(' ');
-    }
-    session.send('Sending post');
-    micropub(session, session.dialogData.data).then((card) => {
+    session.sendTyping();
+    micropub.options.micropubEndpoint = session.userData.micropub;
+    micropub.options.token = session.userData.accessToken;
+    micropub.create(session.dialogData.data, 'multipart').then((url) => {
+      const card = getSuccessCard(session, url, session.dialogData.data.content, session.dialogData.data.name);
       session.endDialog(card);
     }).catch((err) => {
       session.send('Uh oh 😯. There was an error sending that');
@@ -263,79 +237,28 @@ bot.dialog('/authenticate', [
     builder.Prompts.text(session, 'What is your domain?');
   },
   (session, results) => {
+    session.sendTyping();
     const userUrl = cleanUrl(results.response);
     session.send(`Ok I'll try to authenticate at ${userUrl}`);
-    request.get(userUrl, (err, response, body) => {
-      if (err) {
-        console.log(err);
-        session.endDialog('There was an error accessing your site 😔');
-      }
-      if (!body) {
-        session.endDialog('Your site did not return a body 😑');
-      }
-
-      Microformats.get({
-        html: body,
-      }, (err, mfData) => {
-        if (err) {
-          console.log(err);
-          session.endDialog('There was an error getting your microformats data 😑');
-        }
-
-        if (mfData && mfData.rels && mfData.rels.authorization_endpoint && mfData.rels.token_endpoint && mfData.rels.micropub) {
-          session.userData.authEndpoint = mfData.rels.authorization_endpoint[0];
-          session.userData.tokenEndpoint = mfData.rels.token_endpoint[0];
-          session.userData.micropub = mfData.rels.micropub[0];
-          session.userData.me = userUrl;
-          var authParams = {
-            me: userUrl,
-            client_id: appConfig.url,
-            redirect_uri: appConfig.url + '/auth',
-            response_type: 'code',
-            scope: 'post',
-          };
-
-          var authUrl = mfData.rels.authorization_endpoint + '?' + qs.stringify(authParams);
-          session.send(`Ok visit this link to authorize me 🔏: ${authUrl}`);
-          builder.Prompts.text(session, 'Paste the code you get back to me ');
-        } else {
-          session.endDialog('Your site seems to be missing a required endpoint');
-        }
-      });
-    });
+    micropub.options.me = userUrl;
+    micropub.getAuthUrl()
+      .then((url) => {
+        session.send(`Ok visit this link to authorize me 🔏: ${url}`);
+        builder.Prompts.text(session, 'Paste the code you get back to me ');
+      })
+      .catch((err) => session.endDialog(err));
   },
   (session, results) => {
     const code = results.response;
-    const form = {
-      me: session.userData.me,
-      code: code,
-      scope: 'post',
-      client_id: appConfig.url,
-      redirect_uri: appConfig.url + '/auth',
-    };
-    request.post(session.userData.tokenEndpoint, {
-      form: form,
-    }, (err, response, body) => {
-      if (err) {
-        console.log(err);
-        session.endDialog('There was an error verifying your code 😔.');
-      }
-
-      if (response && response.statusCode !== 200) {
-        console.log(response.body);
-        session.send('There was an error verifying your code 😔:');
-        session.endDialog(response.body);
-      }
-
-      var bodyData = qs.parse(body);
-
-      if (!bodyData.me) {
-        session.endDialog('Malformed response from authorization server 😔.');
-      }
-
-      session.userData.accessToken = bodyData.access_token;
-      session.endDialog('Ok I am now authenticated and ready to send micropub requests 🎉');
-    });
+    session.sendTyping();
+    micropub.getToken(results.response)
+      .then((token) => {
+        console.log(token);
+        session.userData.accessToken = token;
+        session.userData.micropub = micropub.options.micropubEndpoint;
+        session.endDialog('Ok I am now authenticated and ready to send micropub requests 🎉');
+      })
+      .catch((err) => session.endDialog(err));
   }
 ]);
 
@@ -369,87 +292,19 @@ bot.dialog('/info', [
   },
 ]);
 
-function micropub(session, data) {
-  return new Promise((fulfill, reject) => {
-    const options = {};
-    options.url = session.userData.micropub;
-    options.headers = {
-      Authorization: `Bearer ${session.userData.accessToken}`,
-    };
-    options.form = data;
-    request.post(options, (err, httpResponse, body) => {
-      if (err || httpResponse.statusCode != 201) {
-        console.log(err);
-        console.log('Status code: ' + httpResponse.statusCode);
-        reject(err);
-      } else {
-        let url = '';
-        if (httpResponse && httpResponse.headers) {
-          if (httpResponse.headers.Location) {
-            url = httpResponse.headers.Location;
-          } else if (httpResponse.headers.location) {
-            url = httpResponse.headers.location;
-          }
-        }
-
-        let card = new builder.HeroCard(session).title('Post Successful');
-        if (data.name) {
-          card.subtitle(data.name);
-        }
-        if (data.content) {
-          card.text(data.content);
-        }
-        if (url) {
-          card.tap(builder.CardAction.openUrl(session, url));
-        }
-
-        const response = new builder.Message(session)
-          .textFormat(builder.TextFormat.xml)
-          .attachments([card]);
-
-        fulfill(response);
-      }
-    });
-  });
-}
-
-function getSyndication(session) {
-  return new Promise((fulfill, reject) => {
-    const options = {};
-    options.url = session.userData.micropub;
-    options.headers = {
-      Authorization: `Bearer ${session.userData.accessToken}`,
-    };
-    options.qs = {
-      q: 'syndicate-to',
-    };
-    request.get(options, (err, httpResponse, body) => {
-      if (err || httpResponse.statusCode != 200) {
-        reject();
-      } else {
-        const result = qs.parse(body);
-        fulfill(result['syndicate-to[]']);
-      }
-    });
-  });
-}
-
-function cleanUrl(url) {
-  url = url.trim();
-  if (url.charAt(0) == '<') {
-    url = url.substr(1);
+function getSuccessCard(session, url, text = false, subtitle = false) {
+  let card = new builder.HeroCard(session).title('Post Successful');
+  if (url) {
+    card.tap(builder.CardAction.openUrl(session, url));
   }
-  if (url.charAt(url.length - 1) == '>') {
-    url = url.substring(0, url.length - 1);
+  if (text) {
+    card.text(text);
   }
-  return url;
-}
-
-function cleanText(text, service) {
-  text = emoji.emojify(text);
-  if ('slack' == service) {
-    text = text.replace('<', '');
-    text = text.replace('>', '');
+  if (subtitle) {
+    card.subtitle(subtitle);
   }
-  return text;
+  const response = new builder.Message(session)
+    .textFormat(builder.TextFormat.xml)
+    .attachments([card]);
+  return response;
 }
